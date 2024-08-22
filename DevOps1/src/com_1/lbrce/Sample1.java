58b3fc25a35e9bc8662ef21ff40f28ef48ba076c
bandhichodu <143315602+bandhichodu@users.noreply.github.com>
2024-08-22 10:42:09 +0530

    Update Sample1.java

--- DevOps1/src/com_1/lbrce/Sample1.java
+++ DevOps1/src/com_1/lbrce/Sample1.java
@@ -4,6 +4,13 @@ public class Sample1 {
 	public static void main(String [] args)
 	{
 		System.out.println("Hello World !");
+		System.out.println("Hello Team lead");
 	}
 
+}
+public class Hero{
+	public static void main(String args[])
+	{
+		System.out.println("hello from hero");
+	}
 }
